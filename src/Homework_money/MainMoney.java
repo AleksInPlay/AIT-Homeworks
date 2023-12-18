641e3e2c9335a9d894bf4437367d437d76e8a97f
AIT TR Student Alex <ruskanaleksei@gmail.com>
2023-12-19 00:53:40 +0100

    sending homework 18.12

--- src/Homework_money/MainMoney.java
+++ src/Homework_money/MainMoney.java
@@ -1,6 +1,8 @@
 package Homework_money;
 
 
+import java.util.Arrays;
+
 public class MainMoney {
     public static void main(String[] args) {
 
@@ -11,6 +13,25 @@ public class MainMoney {
         System.out.println(wallet1);
         System.out.println(wallet2);
         System.out.println(wallet3);
+        System.out.println();
+
+        //Variant s massivom
+
+        Money money1 = new Money("paper", "USD", 10);
+        Money money2 = new Money("coin", "eurocent", 2);
+        Money money3 = new Money ("electronic", "Bitcoin", 1);
+        Money money4 = new Money ("paper", "EUR", 50);
+
+        Money[] massMoney1 = {money1, money2};
+        Money[] massMoney2 = {money3, money4};
+
+        Wallet massWallet1 = new Wallet (massMoney1);
+        System.out.println("Wallet 1 contains: " + Arrays.toString(massWallet1.massMoney));
+
+        Wallet massWallet2 = new Wallet (massMoney2);
+        System.out.println("Wallet 2 contains: " + Arrays.toString(massWallet2.massMoney));
+
+
     }
 
 
--- src/Homework_money/Wallet.java
+++ src/Homework_money/Wallet.java
@@ -2,10 +2,14 @@ package Homework_money;
 
 public class Wallet {
     Money money;
+    Money [] massMoney;
 
 
     public Wallet(Money money) {
         this.money = money;
+    }
+    public Wallet(Money[] massMoney){
+        this.massMoney = massMoney;
     }
 
     @Override
